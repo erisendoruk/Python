a7ef55e205e1b542717a4c09bdc1dbe7ba7723db
erisendoruk <doruk.erisen@gmail.com>
2017-05-13 01:53:07 +0100

    Deleting login.py

--- login.py
+++
@@ -1,9 +0,0 @@
-from twill.commands import *
-
-
-go('https://members.thirdspace.london/#/login')
-
-fv("1", "email-email", "doruk.erisen@gmail.com")
-fv("1", "password-clear", "101905gs")
-
-submit('0')
